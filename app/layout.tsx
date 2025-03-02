cdce60c6ae408d242d22ce61134d6921b5186613
Ajey <ajejey@gmail.com>
2025-03-02 22:25:18 +0530

    add google adsense tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -103,6 +103,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-adsense-account" content="ca-pub-3613850686549619"/>
         <Script 
           async 
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3613850686549619" 
